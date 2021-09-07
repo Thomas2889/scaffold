8a2859a3b25cb6540c109b64d49466159ad36d04
Thomas2889 <thomashollinshead@btinternet.com>
2021-09-07 15:52:20 +0100

    Some changes

--- Scaffold/Renderer/Renderer/Renderer.cpp
+++ Scaffold/Renderer/Renderer/Renderer.cpp
@@ -3,7 +3,8 @@
 #include <GL/glew.h>
 #include <CppLog/Logger.h>
 
-#include "Core/Window/WindowManager.h"
+#include <Core/Window/WindowManager.h>
+#include <Core/Ecs/SystemProto.h>
 
 #include "Exceptions/RenderingExceptions.h"
 
@@ -41,7 +42,82 @@ namespace scaffold::renderer
 				break;
 			}
 		}
+
+
+		class RendererSystem : public ecs::SystemProto
+		{
+			DEFINE_SYSTEM_PROTO(RendererSystem);
+
+			GLuint VAO;
+			GLuint VBO;
+			GLuint shaderProgram;
+
+			System_Start()
+			{
+				float vertices[] = {
+					-0.5f, -0.5f, 0.0f,
+					 0.5f, -0.5f, 0.0f,
+					 0.0f,  0.5f, 0.0f
+				};
+
+				glGenVertexArrays(1, &VAO);
+				glBindVertexArray(VAO);
+
+				glGenBuffers(1, &VBO);
+				glBindBuffer(GL_ARRAY_BUFFER, VBO);
+				glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+				const char* vertexShaderSource = "#version 330 core\n"
+					"layout (location = 0) in vec3 aPos;\n"
+					"void main()\n"
+					"{\n"
+					"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+					"}\0";
+
+				const char* fragmentShaderSource = "#version 330 core\n"
+					"out vec4 FragColor;\n"
+					"void main()\n"
+					"{\n"
+					"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+					"}\0";
+
+				GLuint vertexShader;
+				vertexShader = glCreateShader(GL_VERTEX_SHADER);
+				glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+				glCompileShader(vertexShader);
+
+				GLuint fragmentShader;
+				fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+				glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+				glCompileShader(fragmentShader);
+
+				shaderProgram = glCreateProgram();
+				glAttachShader(shaderProgram, vertexShader);
+				glAttachShader(shaderProgram, fragmentShader);
+				glLinkProgram(shaderProgram);
+
+				glUseProgram(shaderProgram);
+				glDeleteShader(vertexShader);
+				glDeleteShader(fragmentShader);
+
+				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+				glEnableVertexAttribArray(0);
+			}
+
+			System_Render()
+			{
+				glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+				glClear(GL_COLOR_BUFFER_BIT);
+
+				glUseProgram(shaderProgram);
+				glBindVertexArray(VAO);
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+
+				SDL_GL_SwapWindow(window::GetInstance(window::mainWindow)->getWindow());
+			}
+		};
 	}
+	IMPLEMENT_SYSTEM_PROTO_PRI(RendererSystem, -100);
 
 	void InitRenderer()
 	{
